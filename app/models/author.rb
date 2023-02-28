9e2817babbc54cc35a2d64e85d212509058058f3
kpokorski <kfpokorski@gmail.com>
2023-02-28 10:57:47 +0100

    validations added

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :author
+  has_one_attached :cover
+
+  validates :title, presence: true
+  validates :publishing_house, presence: true
+  validates :author_id, presence: true
+  validates :cover, content_type: ["image/png", "image/jpeg"], presence: true
 end
