3f58dbd1d116e4a3fbe35f4d08cebe661c23b40a
kpokorski <kfpokorski@gmail.com>
2023-03-01 10:59:32 +0100

    seeds added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  scope path: "/api/v1" do
-    resources :authors
-    resources :books
+  namespace :api do
+    namespace :v1 do
+      resources :authors
+      resources :books
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'ffaker'
+
+100.times do
+  author = Author.create(
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    email: FFaker::Internet.email,
+    phone_number: rand(100000000..999999999)
+  )
+  author.save
+end
+
+100.times do
+  book = Book.create(
+    tittle: FFaker::Name.first_name,
+    year: rand(1500..2023),
+    author_id: rand(1..100),
+    ISBN_number: rand(100000000..999999999),
+    publishing_house: FFaker::Company.name,
+    description: FFaker::Lorem.sentence
+  )
+  book.save
+end
+
+
